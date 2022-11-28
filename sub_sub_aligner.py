bc599d0a0f953b9d3a86e904c305ea51df9c68c8
basrizk <basem.rizk@outlook.com>
2022-11-28 09:56:08 -0800

    fix non spanning removal

--- sub_sub_aligner.py
+++ sub_sub_aligner.py
@@ -6,33 +6,32 @@ class SubSubAligner(Aligner):
     
     def _get_overlapping_snippets(self, sub_win: SubtitleWindow, debug):
         overlapping_matches = []
-        while True:
-            cur_snippet = self._get_cur_sub_snippet()
+        while self._get_cur_sub_snippet():
             # if debug:
             #     print('\n Current Snippet:\n', cur_snippet)
             
             # Get overlapping subtitle snippets
-            if sub_win.is_overlaping(cur_snippet):
-                overlapping_matches.append(cur_snippet)
+            if sub_win.is_overlaping(self._get_cur_sub_snippet()):
+                overlapping_matches.append(self._get_cur_sub_snippet())
             elif len(overlapping_matches) > 0:
                 self._shift_back_sub_snippet()
                 break
             # elif debug:
             #     breakpoint()
             
-            if not self._get_next_sub_snippet():
-                break
+            self._get_next_sub_snippet()
+            
         return overlapping_matches
     
-    def _remove_spanning_snippets(self, snippets: SubtitleSnippet):
+    def _remove_spanning_snippets(self, snippets: SubtitleSnippet):            
         if len(snippets) <= 1:
             return snippets
-        
+
         kept = []
         for s1 in snippets:
             remove = True
             for s2 in snippets:
-                if not s1.is_overlaping(s2):
+                if not s1.timestamp.is_spanning(s2.timestamp):
                     remove = False
             if not remove:
                 kept.append(s1)
@@ -48,7 +47,7 @@ class SubSubAligner(Aligner):
         return kept_wins
             
             
-        
+    
     
     def _get_corresponding_text(self, sub_win: SubtitleWindow, debug):
         if debug:
@@ -59,13 +58,15 @@ class SubSubAligner(Aligner):
         if overlapping_snippets:
 
             # Remove spanning snippets (Advertisement or Author..or such..) 
-            overlapping_snippets = self._remove_spanning_snippets(overlapping_snippets)
+            overlapping_nonspanning_snippets = self._remove_spanning_snippets(overlapping_snippets)
 
             # Pick statements based on line numbers and number of snippets in sub_win
-            overlapping_wins = self._remove_non_overlapping_lines(sub_win, overlapping_snippets)
+            overlapping_wins = self._remove_non_overlapping_lines(sub_win, overlapping_nonspanning_snippets)
+            
+            self.most_recent_successful_snippet = overlapping_wins[-1].es.get_id()
+
             # print('Non-spanning Wins:\n', overlapping_wins)                        
             matched_text = ' '.join([w.u for w in overlapping_wins])
-            
             
             if debug:
                 print('Overlapping Snippets Ids:', list(map(lambda x: x.id, overlapping_snippets)))
@@ -78,20 +79,35 @@ class SubSubAligner(Aligner):
             return matched_text
         else:
             print('Failed to find any overlapping snippets with snippet')
-            breakpoint()
-            return None 
+            # breakpoint()
+            # Return for now the text as is in English (Assuming it refers to sounds!)
+            return sub_win.u 
         
         
     
     def find_alignment(self, sub_wins, debug=False):
         # Set pointer over the relevant episode
         self.open_episode(sub_wins[0].ss.episode_title)
-        
         text_matches = []
+        self.most_recent_successful_snippet = None
         for sub_win in sub_wins:
             matched_text = self._get_corresponding_text(
                 sub_win, debug=debug
             )
+            if matched_text == sub_win.u:
+                if self.most_recent_successful_snippet:
+                    self.open_episode(
+                        sub_win.ss.episode_title, 
+                        snippet_id=self.most_recent_successful_snippet
+                    )
+                else:
+                    break
             text_matches.append(matched_text)
+         
+         
+        # from subtitles_utils.utils import prepare_txt
+        # print([prepare_txt(t) for t in text_matches])
+        # breakpoint()
+
         print('Finished Conversation Windows')
         return text_matches
